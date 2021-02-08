6dfd66e74cbf78a65c067b08489de15bd0f2326a
Quentin DUCASSE <quentin.ducasse@ensta-bretagne.org>
2021-02-08 17:17:52 +0100

    Changed syntax

--- run_sdvu.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import glob
-
-if __name__ == "__main__":
-    sdve_files = glob.glob("**/*.sdve", recursive=True)
-    for file in sdve_files:
-        subprocess.call("sdvu " + file, shell=True)
