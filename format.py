e74348fbdf236d85b57a644d1d5b94b37c0acf13
Quentin DUCASSE <quentin.ducasse@ensta-bretagne.org>
2021-04-20 14:30:10 +0200

    Execution simulation stats added

--- format.py
+++ format.py
@@ -1,22 +1,51 @@
 import os
 import glob
+import subprocess
+
+error_arr = ["blocks", "driving_phils", "telephony", "mcs"]
+error_shl = ["synapse"]
+error_biz = ["loyd"]
+
+error_sim = ["bridge", "cambridge", "collision", "extinction",
+             "frogs", "hanoi", "lifts", "needham", "peterson",
+             "protocols", "rushhour", "sokoban"]
 
 def process_files():
     sdve_files = glob.glob("**/*.sdve", recursive=True)
-    sdve_files = [sdve_file for sdve_file in sdve_files if "prop" in sdve_file]
+    sdve_files = [sdve_file for sdve_file in sdve_files if not("prop" in sdve_file) and "telephony." in sdve_file]
 
     for sdve_file in sdve_files:
-        print(sdve_file)
-        lines = open(sdve_file, 'r').readlines()
-        if "//" in lines[-2]:
-            continue
-        else:
-            new_minus2 = "      // " + lines[-2].lstrip()
-            new_minus1 = "      // " + lines[-1].lstrip()
-            lines[-2] = new_minus2
-            lines[-1] = new_minus1
-            open(sdve_file, 'w').writelines(lines)
+        for ncores in range(1,10):
+            print(sdve_file)
+            subprocess.check_output(["python", "../sdvs/sdvs/sdvs.py",
+                                     "-s", sdve_file,
+                                      "--ncores", str(ncores),
+                                      "--cfgsize", str(determine_cfg_size(sdve_file)),
+                                      "--cfg", str(0),
+                                      "--outputfile", "exec_stats.csv"
+                              ])
+
+def determine_cfg_size(sdve_file):
+    with open(sdve_file) as f:
+        content = f.readlines()
+        content = [line.strip() for line in content] # Remove trailing and leading spaces
+        content = [line for line in content if line] # Remove empty strings
+
+    cfg_size = 0
+    i = 0
+    while not(content[i].startswith("process")):
+        if content[i].startswith("bool"):
+            cfg_size += 8
+        elif content[i].startswith("byte"):
+            cfg_size += 8
+        elif content[i].startswith("state"):
+            cfg_size += 16
+        elif content[i].startswith("int"):
+            cfg_size += 32
+        i+=1
+    return cfg_size
+
 
 if __name__ == "__main__":
-    # process_file("anderson/anderson.1.prop2.sdve")
+    # print(determine_cfg_size("adding/adding.1.sdve"))
     process_files()
